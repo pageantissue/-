f93ed28a40a733a99beb933f96d4048fa4b3a770
Yoshioen <jingfeng_deng@outlook.com>
2023-12-29 16:00:18 +0800

    Final Submit

---
+++ 源码/src/BusinessLogic/Admin.java
@@ -0,0 +1,14 @@
+package BusinessLogic;
+public class Admin extends Account
+{
+    public Admin(String name, String surname, int ID)
+    {
+        super(name, surname, ID);
+    }
+
+    //setUserStatus
+    //acceptOrder
+    //checkOrders
+    //addNewFood
+    //changeExistingFood
+}
